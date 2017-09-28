463c7ef8f02fad7db081f9306c3f40f7fa1d4d7d
Wayne Cassidy <wayne.cassidy@alumni.carleton.ca>
2017-09-27 22:00:16 -0700

    Corrected some css issues and added the html for the main AlgaeCal Plus page (without the carousel)

---
+++ algaecal_product_page_content.php
@@ -0,0 +1,91 @@
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6 "><img src="http://127.0.0.1/wp/wp-content/uploads/2017/09/AP-1-300x300.png" alt="AlgaeCal Plus Bottle" /></div>
+		<div class="col-sm-6">
+			<div class="row">
+				<div class="col-12"><h1 class="product-heading">AlgaeCal Plus</h1></div>
+				<div class="col-12 hidden-xs"><h2>A proven, doctor-approved 100% natural solution for women with osteoporosis</h2></div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<span class="fa fa-star star-filled"></span><span class="fa fa-star star-filled"></span><span class="fa fa-star star-filled"></span><span class="fa fa-star star-filled"></span><span class="fa fa-star star-filled"></span>225 Reviews</div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<dl>
+						<dt class="checked">Plant-Based</dt>
+						<dd>Super absorbable calcium, magnesium and trace minerals from algae!</dd>
+						<dt class="checked">Guarantee</dt>
+						<dd>To increase bone density in 6 months when taken with Strontium Boost!</dd>
+						<dt class="checked">Clinical Doses</dt>
+						<dd>Of vitamins D3, K2, boron and magnesium!</dd>
+					</dl>
+				</div>
+			</div>
+			<div class="row product-description hidden-xs">
+				<div class="col-12">
+					<table>
+						<tr>
+							<th></th>
+							<th>AlgaeCal Plus</th>
+						</tr>
+						<tr>
+							<td>Description:</td>
+							<td>90 easy to swallow veggie capsules</td>
+						</tr>
+						<tr>
+							<td>Suggested Use:</td>
+							<td>4 caps daily (2 caps twice a day with meals)</td>
+						</tr>
+						<tr>
+							<td>Availability:</td>
+							<td class="blue-highlight">IN STOCK leaves warehouse in 1 business day</td>
+						</tr>
+						<tr>
+							<td>Shipping Fee:</td>
+							<td class="green-u-highlight">Free Shipping Everywhere</td>
+						</tr>
+					</table>
+				</div>
+			</div>				
+			<div class="row external-links">
+				<div class="col-sm-5 col-xs-12"><a class="friend" href="#">Tell a Friend</a>
+				</div>
+				<div class="col-sm-7 col-xs-12"><a class="doctor" href="#">Take This to Your Doctor</a>
+				</div>
+			</div>				
+		</div>
+	</div>
+	<div class="row product-row">
+		<div class="col-xs-12 col-md-4 product-box">
+			<div class="product-title"><h3>1 Bottle</h3></div>
+			<div class="product-perday-price">Only <span class="currency"><span class="dollar-sign">$</span>2.22</span> per Day!</div>
+			<div class="product-image"><img src="http://127.0.0.1/wp/wp-content/uploads/2017/09/AP-1-300x300.png" alt="AlgaeCal Plus Bottle" /></div>
+			<div class="product-price"><span class="currency"><span class="dollar-sign">$</span>50.00</span> per Bottle</div>
+			<div class="product-today-price"><strong>Today's Price: </strong><span class="dollar-sign">$</span>50 <span class="denomination">USD</span></div>
+			<div class="add-cart-button"><a href="#" class="button"><img class="seal" src="http://127.0.0.1/wp/wp-content/uploads/2017/09/seal.png" />ADD TO CART</a></div>
+		</div>
+		<div class="col-xs-12 col-md-4 product-box">
+			<div class="discount-label"><span class="discount-amount">15%</span><br>OFF</div>
+			<div class="product-title"><h3>3 Month Supply</h3></div>
+			<div class="product-subtitle">4 Bottles</div>
+			<div class="product-perday-price">Only <span class="currency"><span class="dollar-sign">$</span>1.89</span> per Day!</div>
+			<div class="product-image"><img src="http://127.0.0.1/wp/wp-content/uploads/2017/09/AP-8-300x300.png" alt="AlgaeCal Plus 4 Bottles" /></div>
+			<div class="product-price"><span class="currency"><span class="dollar-sign">$</span>43.00</span> per Bottle</div>
+			<div class="product-retail-price"><strong>Retail Price: </strong><span class="dollar-sign">$</span><span class="strike-out">200</span></div>
+			<div class="product-today-price"><strong>Today's Price: </strong><span class="dollar-sign">$</span>170 <span class="denomination">USD</span></div>
+			<div class="add-cart-button"><a href="#" class="button"><img class="seal" src="http://127.0.0.1/wp/wp-content/uploads/2017/09/seal.png" />ADD TO CART</a></div>
+		</div>
+		<div class="col-xs-12 col-md-4 product-box">
+			<div class="discount-label"><span class="discount-amount">30%</span><br>OFF</div>
+			<div class="product-title"><h3>6 Month Supply</h3></div>
+			<div class="product-subtitle">8 Bottles</div>
+			<div class="product-perday-price">Only <span class="currency"><span class="dollar-sign">$</span>1.54</span> per Day!</div>
+			<div class="product-image"><img src="http://127.0.0.1/wp/wp-content/uploads/2017/09/AP-8-300x300.png" alt="AlgaeCal Plus 8 Bottles" /></div>
+			<div class="product-price"><span class="currency"><span class="dollar-sign">$</span>35.00</span> per Bottle</div>
+			<div class="product-retail-price"><strong>Retail Price: </strong><span class="dollar-sign">$</span><span class="strike-out">400</span></div>
+			<div class="product-today-price"><strong>Today's Price: </strong><span class="dollar-sign">$</span>277 <span class="denomination">USD</span></div>
+			<div class="add-cart-button"><a href="#" class="button"><img class="seal" src="http://127.0.0.1/wp/wp-content/uploads/2017/09/seal.png" />ADD TO CART</a></div>
+		</div>
+	</div>
+</div>
