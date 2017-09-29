7d3dedf0b3691921440889b602d61a1348b9bbd8
Wayne Cassidy <wayne.cassidy@alumni.carleton.ca>
2017-09-29 12:30:21 -0700

    Finished the php file and java script file for the plugin

--- wp-content/plugins/algaecal-carousel/algaecal-carousel.php
+++ wp-content/plugins/algaecal-carousel/algaecal-carousel.php
@@ -1,4 +1,5 @@
 <?php
+// Make sure people aren't reading this file from the web (web server permissions should be used so this is just a backup)
 defined( 'ABSPATH' ) or die( 'This file is not for reading!' );
 
 /*
@@ -10,16 +11,29 @@ License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 */
 
+// Add all of the css and js files needed by this plugin
 function add_styles_scripts()
 {
-	// Add the main plugin css
-	wp_enqueue_style( 'algaecal_carousel_style', plugin_dir_url( __FILE__ ) . 'algaecal-carousel-style.css', array ( 'bootstrap_css' ));
+	// Add slick css files
+	wp_enqueue_style( 'slick_css', plugin_dir_url( __FILE__ ) . 'slick/slick.css');
+	wp_enqueue_style( 'slick_theme_css', plugin_dir_url( __FILE__ ) . 'slick/slick-theme.css');
 
-	// Bootstrap js which requires JQuery and we want to use jquery 3.2.1 from the google CDN
-	//wp_enqueue_script( 'bootstrap_js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array ( 'bootstrap_css' ), '3.3.7', true);	
+	// Add the main plugin css
+	wp_enqueue_style( 'algaecal_carousel_style', plugin_dir_url( __FILE__ ) . 'algaecal-carousel-style.css');
+
+
+	// Load slick js
+	wp_enqueue_script( 'slick_js', plugin_dir_url( __FILE__ ) . 'slick/slick.min.js', array ( 'jquery' ), '1.0', true);
+
+	// Load main js for plugin
+	wp_enqueue_script( 'algaecal_carousel_script', plugin_dir_url( __FILE__ ) . 'algaecal_carousel.js', array ( 'jquery' ), '1.0', true);
+
+	// Load main wistia script
+	wp_enqueue_script( 'ev1_js', 'https://fast.wistia.com/assets/external/E-v1.js', false, '1.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'add_styles_scripts' );
 
+// Setup the custom post_type
 function create_algaecal_carousel()
 {
 	// Set up the labels to be used in the admin console
@@ -56,30 +70,68 @@ function create_algaecal_carousel()
 }
 add_action( 'init', 'create_algaecal_carousel' );
 
+// The main "GUTS" of this plugin.  This method responds to the usage of a shortcode placed on a page.
 function display_algaecal_carousel($atts)
 {
-	// Make sure the name was set.  If not then print out help usage.
-	$return_val = "<h4>Could not find algaecal_carousel requested</h4>";
+	// Default return value to use if there name of the algaecal_carousel is invalid or it doesn't contain any media
+	$return_val = "<h4>Could not find algaecal_carousel requested or there were no media entries</h4>";
 	
-	// Set a default name to test
+	// Set a default name to test against
 	$atts = shortcode_atts( array(
 		'name' => 'no_name'
 	), $atts, 'algaecal_carousel' );
 	
-	// Retrive the algaecal_carousel page requested
+	// Retrieve the algaecal_carousel page requested
 	$requested_page = get_page_by_title($atts['name'], OBJECT, 'algaecal_carousel');
 	
+	// If the page is invalid then we just don't do anything as the default return value is already set
 	if( $atts['name'] != 'no_name' && !is_null( $requested_page ) )
 	{
+		// NOTE: I was going to use a generated thumbnail however the wistia site specified that thumbnail
+		// generation can take up to 20 seconds.  Instead I have the person who makes the algaecal_carousel post_type
+		// enter a thumbnail url as a custom field.  This way they can pre-generate the thumbnail for speed or use
+		// an auto generate url if they don't mind the speed issue
+
+		// ALSO NOTE: The custom fields are setup so that in wordpress, if the media_type is image,
+		// then the wistia_key and wistia_thumbnail fields are not displayed and, if the media_type is video,
+		// then the image field is not displayed
 		
-		$return_val =  '<div class="row">
-							<div class="col-12 carousel-image">
-								<h4>' . $requested_page->post_title . ' was requested</h4>
-								<img src="http://127.0.0.1/wp/wp-content/uploads/2017/09/AP-1-300x300.png" alt="AlgaeCal Plus Bottle" />
+		// Get all of the media entries from the ACF Repeater field (get all the rows)
+		$media_entries = get_field('media_entries',  $requested_page);
+		if($media_entries)
+		{
+			// Add the carousel pre-amble tags
+			$return_val =  '<div class="hidden-xs algaecal-carousel-preview">
+								<img id="algaecal-carousel-image" class="algaecal-carousel-image" data-video-url="" />
 							</div>
-						</div>';
+							<div id="algaecal-carousel" class="algaecal-carousel">';
+
+			foreach( $media_entries as $media_entry )
+			{
+				// Read the custom fields for the algaecal_carousel post_type
+				$caption = $media_entry['caption'];
+				$media_type = $media_entry['media_type'];;
+				$image = $media_entry['image'];;
+				$wistia_key = $media_entry['wistia_key'];;
+				$wistia_thumbnail = $media_entry['wistia_thumbnail'];;
+										
+				// Add the appropriate thumbnail image
+				if( $media_type == 'image' )
+				{
+					$return_val .= '<img class="algaecal-carousel-item" data-video-key="" src="' . $image .
+								   '" alt="' . $caption . '" />';
+				}
+				else
+				{
+					$return_val .= '<img class="algaecal-carousel-item" data-video-key="' . $wistia_key . 
+								   '" src="' . $wistia_thumbnail . '" alt="' . $caption . '" />';
+				}
+			}
+			
+			$return_val .= '</div>';
+		}
 	}
-	
+
 	return $return_val;
 }
 add_shortcode( 'algaecal_carousel', 'display_algaecal_carousel' );
--- wp-content/plugins/algaecal-carousel/algaecal_carousel.js
+++ wp-content/plugins/algaecal-carousel/algaecal_carousel.js
@@ -1,5 +1,8 @@
+// Java script needed for the carousel and lightbox functionality
 
+// Everything in the ready event otherwise multi-threaded renderers may have dom issues
 $(document).ready(function(){
+	// Setup the slick carousel and deal with small screens
 	$('.algaecal-carousel').slick({
 		infinite: true,
 		slidesToShow: 3,
@@ -17,12 +20,12 @@ $(document).ready(function(){
 		]
 	});
 	
-	$("body").prepend('<div id="lightbox" class="lightbox"><img id="lightbox-item" class="lightbox-item" /></div>');
+	// Need to add the lightbox to the page at load time since this is for a wordpress plugin
+	// Don't want users to have to add tags to support the plugin
+	$("body").prepend('<div id="lightbox" class="lightbox"></div>');
 	
-	$("#lightbox").click(function(event) {
-		$("#lightbox").css("display", "none");
-	});
-
+	// Method to change the preview image above the carousel when the user clicks one of the thumbnails
+	// in the carousel
 	$("#algaecal-carousel").click(function(event) {
 		var target = $( event.target );
 		
@@ -31,29 +34,52 @@ $(document).ready(function(){
 			// copy the attributes of the image tag from the carousel to the preview image
 			$("#algaecal-carousel-image").attr( "src", target.attr("src") );
 			$("#algaecal-carousel-image").attr( "alt", target.attr("alt") );
-			$("#algaecal-carousel-image").data( "video-url", target.data("video-url") );
+			$("#algaecal-carousel-image").data( "video-key", target.data("video-key") );
 		}
 	});
 
+	// Display the lightbox with either the image or the wistia video when the image preview is clicked on
 	$("#algaecal-carousel-image").click(function(event) {
 		var target = $( event.target );
 
 		if( target.is( "img" ) )
 		{
-			var video = $(target).data("video-url");
+			// If the video_key (wistia_key) is empty then this is an image not a video
+			var video_key = $(target).data("video-key");
 		
-			if(video == '')
+			if(video_key == '')
 			{
+				// Add the image to the lightbox and display
+				$("#lightbox").prepend('<img id="lightbox-image" class="lightbox-image" />');
 				$("#lightbox").css("display", "block");
-				$("#lightbox-item").attr( "src", target.attr("src") );
+				$("#lightbox-image").attr( "src", target.attr("src") );
 			}
 			else
 			{
-			
+				// Add the wistia video embed tags and display the light box
+				$("#lightbox").css("display", "block");
+				var video_html = '<div id="lightbox-video" class="lightbox-video">' + 
+								 '<script src="//fast.wistia.com/embed/medias/' + video_key + 
+								 '.jsonp" async=""></script><div class="wistia_embed wistia_async_' + video_key + 
+								 '" style="height:395px;width:700px">&nbsp;</div></div>';
+				$("#lightbox").prepend(video_html);
 			}
 		}
 	});
-	
+
+	// Shut down the lightbox when clicked
+	$("#lightbox").click(function(event) {
+		var target = $( event.target );
+
+		// If the user clicks outside of the image or video shut down the light box and remove the image or video
+		if( target.is( "div" ) )
+		{
+			$("#lightbox").css("display", "none");
+			$("#lightbox-image").remove();
+			$("#lightbox-video").remove();
+		}
+	});
+
 	// Display the first image from the carousel in the image preview
 	$("#algaecal-carousel img[data-slick-index='0']").trigger("click");
 });
